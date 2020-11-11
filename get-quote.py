2054da97148af9e06dea71ba678adae135ba3ff4
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2020-11-11 21:39:05 +0100

    random quot bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,11 @@
+import random
 def primary():
-    print("keep it logically awesome")
-
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    last = len(quotes)-1        #updates code automaticaly if quotes file is modified
+    rnd = random.randint (0, last)  #choose a random value in an interval and atributes it to the variable rnd
+    print(quotes[rnd])
 
 if __name__== "__main__":
     primary()
